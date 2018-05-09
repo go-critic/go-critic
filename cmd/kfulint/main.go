482ab11b92986145c9ae8d2a07298e06fd7ec6fd
Alekseev Artem <a.artem060@gmail.com>
2018-05-09 22:52:13 +0300

    fix

--- cmd/kfulint/main.go
+++ cmd/kfulint/main.go
@@ -112,7 +112,7 @@ func (l *linter) InitCheckers() {
 		{"param-name", lint.NewParamNameChecker(l.ctx)},
 		{"type-guard", lint.NewTypeGuardChecker(l.ctx)},
 		{"parenthesis", lint.NewParenthesisChecker(l.ctx)},
-		{"underef", lint.NewUnderedChecker(l.ctx)},
+		{"underef", lint.NewUnderefChecker(l.ctx)},
 		{"param-duplication", lint.NewParamDuplicationChecker(l.ctx)},
 	}
 }
