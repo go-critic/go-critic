f88cb805080b7a5ae53a512d14ee164e856cfe09
Alekseev Artem <a.artem060@gmail.com>
2018-05-10 23:28:12 +0300

    add tests for underef_checker (#64)

--- cmd/kfulint/testdata/underef/checker_tests.go
+++ cmd/kfulint/testdata/underef/checker_tests.go
@@ -1,21 +1,38 @@
 package checker_tests
 
-type t struct {
-	a  int
-	t2 struct {
-		b int
+type sampleStruct struct {
+	field        int
+	nestedStruct struct {
+		nestedField int
 	}
 }
 
-func (p *t) c() {
+type sampleInterface interface {
+	method()
 }
 
-func sample() {
-	var k *t
-	///: could simplify (*k).a to k.a
-	(*k).a = 5
-	///: could simplify (*k).c to k.c
-	(*k).c()
-	///: could simplify (*k).t2 to k.t2
-	(*k).t2.b = 6
+func (s *sampleStruct) method() {
+}
+
+func sampleCase() {
+	var k *sampleStruct
+	///: could simplify (*k).field to k.field
+	(*k).field = 5
+	//TODO: could simplify (*k).method() to k.method()
+	///: could simplify (*k).method to k.method
+	(*k).method()
+	//TODO: could simplify (*k).nestedStruct.nestedField to k.nestedStruct.nestedField
+	///: could simplify (*k).nestedStruct to k.nestedStruct
+	(*k).nestedStruct.nestedField = 6
+}
+
+func sampleCase2(k sampleInterface) {
+	k.(*sampleStruct).method()
+}
+
+func sampleCase3() {
+	var k *[5]int
+
+	//TODO: could simplify (*k)[5] to k[5]
+	(*k)[2] = 3
 }
