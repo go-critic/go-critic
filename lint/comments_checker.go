f196eb1982894d8468aeacb0ad284ec86cf913ff
weeellz <weeellz12@gmail.com>
2018-05-26 21:48:02 +0300

    Fix.

--- lint/comments_checker.go
+++ lint/comments_checker.go
@@ -1,12 +1,3 @@
-// new checker:
-// 1. create file lint/foo_checker.go
-// 2. choose base implementation from wrappers.go
-// 3. define checker type that embeds base implementation
-// 4. declare constructor for check function
-// 5. add checker entry to checkFunctions from lint/lint.go
-// 6. add tests in cmd/kfulint/testdata/foo/checker_tests.go
-// 7. implement checker
-
 package lint
 
 import (
@@ -26,16 +17,9 @@ func commentsCheck(ctx *context) func(*ast.File) {
 }
 
 func (c *commentsChecker) CheckFile(f *ast.File) {
-	// 1. loop over f.Decls
-	// 2. find func decls
-	// 3. for each funcdecl check decl.Doc
-	// 4. if decl.Doc is not nil, check its text
-
 	for _, decl := range f.Decls {
 		if decl, ok := decl.(*ast.FuncDecl); ok {
 			if decl.Doc != nil {
-				// fmt.Println(decl.Doc.Text())
-				//fmt.Printf("%q\n", decl.Doc.List[0].Text)
 				if c.regexp.MatchString(decl.Doc.List[0].Text) {
 					c.warn(decl)
 				}
@@ -47,20 +31,3 @@ func (c *commentsChecker) CheckFile(f *ast.File) {
 func (c *commentsChecker) warn(decl *ast.FuncDecl) {
 	c.ctx.Warn(decl, "comment should contain function name")
 }
-
-/*
-
-// decl.Doc
-	var decl *ast.FuncDecl
-	decl.Doc.Text() // => comment text
-
-// Foo ...
-func Foo() {
-}
-
-"Foo ..."
-
-decl.Doc.Text() == decl.Name.Name + " ..."
-"Foo ..." == "Foo ..."
-
-*/
