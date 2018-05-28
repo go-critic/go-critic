b98b13bb74b2dc8e169a7060751a550b68bd74e1
Iskander Sharipov <iskander.sharipov@intel.com>
2018-05-28 18:51:08 +0300

    lint: fix builtin-shadow checker panic (#118)
    builtin-shadow checker panics when assign stmt LHS
    is not *ast.Ident.
    
    This can be reproduced by running it over GOROOT using lintwalk.
    
    This patch fixes an issue and adds tests that check that
    this issue remains fixed.

--- cmd/kfulint/testdata/builtin-shadow/checker_tests.go
+++ cmd/kfulint/testdata/builtin-shadow/checker_tests.go
@@ -1,5 +1,17 @@
 package checker_test
 
+func noWarnigs() {
+	var foo struct {
+		len int
+		cap int
+	}
+
+	foo.len = 123
+	foo.cap = 321
+
+	foo.len, foo.cap = foo.cap, foo.len
+}
+
 func assigningToBuiltinIdentifiers() {
 	// Types:
 	/// assigning to predeclared identifier: bool
