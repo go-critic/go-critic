10f6712b8dcf36d1645130866d60909d812b841d
Alekseev Artem <a.artem060@gmail.com>
2018-06-03 16:42:47 +0300

    cmd/docgen: implement documentation generator (#132)

---
+++ cmd/makedocs/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"text/template"
+
+	"github.com/PieselBois/kfulint/lint"
+)
+
+const (
+	docsPath      = "../../docs/"
+	checkersPath  = docsPath + "checkers/"
+	templatesPath = docsPath + "templates/"
+)
+
+type checker struct {
+	Name         string
+	Description  string
+	Experimental bool
+}
+
+func main() {
+	tmpl, err := template.ParseFiles(templatesPath + "overview.md.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+	checkers := []checker{}
+	for _, r := range lint.RuleList() {
+		desc, err := getDesc(r.Name())
+		if err != nil {
+			log.Fatal(r.Name())
+		}
+		checkers = append(checkers, checker{
+			Name:         r.Name(),
+			Experimental: r.Experimental(),
+			Description:  desc,
+		})
+	}
+	buf := bytes.Buffer{}
+	err = tmpl.Execute(&buf, struct {
+		Checkers []checker
+	}{
+		Checkers: checkers,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := ioutil.WriteFile(docsPath+"overview.md", buf.Bytes(), 0600); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getDesc(name string) (string, error) {
+	b, err := ioutil.ReadFile(checkersPath + name + ".md")
+	return string(b), err
+}
--- docs/checkers/builtin-shadow.md
+++ docs/checkers/builtin-shadow.md
@@ -2,11 +2,7 @@ Detects when
 [predeclared identifiers](https://golang.org/ref/spec#Predeclared_identifiers)
 shadowed in assignments.
 
-Avoid situations when using this identifiers like local variables.
-Later you can add code trying to access this identifier like a language identifier.
-It can take you some time before you figuring it out.
-
-**Before**
+**Before:**
 ```go
 func main() {
     // shadowing len function
@@ -15,7 +11,7 @@ func main() {
 }
 ```
 
-**After**
+**After:**
 ```go
 func main() {
     // change identificator name
