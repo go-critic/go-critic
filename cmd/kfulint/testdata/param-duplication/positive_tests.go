86d200d9808aa27a6b6d570ad7cb403ed9db8cc1
Alekseev Artem <a.artem060@gmail.com>
2018-06-05 00:35:06 +0300

    lint/param-duplication: get correct ast node (#137)

--- cmd/kfulint/testdata/param-duplication/positive_tests.go
+++ cmd/kfulint/testdata/param-duplication/positive_tests.go
@@ -1,38 +1,37 @@
 package checker_test
 
-/// a int, b int, c int could be replaced with a, b, c int
+/// func(a int, b int, c int) could be replaced with func(a, b, c int)
 func simple1(a int, b int, c int) {}
 
-/// a int, b int could be replaced with a, b int
+/// func() (a int, b int) could be replaced with func() (a, b int)
 func simple2() (a int, b int) { return 0, 0 }
 
-/// a int, b int, c int could be replaced with a, b, c int
+/// func() (a int, b int, c int) could be replaced with func() (a, b, c int)
 func simple3() (a int, b int, c int) { return 0, 0, 0 }
 
-/// a, b int, c int could be replaced with a, b, c int
+/// func(a, b int, c int) could be replaced with func(a, b, c int)
 func mixedStyle1(a, b int, c int) {}
 
-/// a, b int, c, d int could be replaced with a, b, c, d int
+/// func(a, b int, c, d int) could be replaced with func(a, b, c, d int)
 func mixedStyle2(a, b int, c, d int) {}
 
-/// a, b, c int, d int could be replaced with a, b, c, d int
+/// func(a, b, c int, d int) could be replaced with func(a, b, c, d int)
 func mixedStyle3(a, b, c int, d int) {}
 
-/// a int, b, c, d int could be replaced with a, b, c, d int
+/// func(a int, b, c, d int) could be replaced with func(a, b, c, d int)
 func mixedStyle4(a int, b, c, d int) {}
 
-/// a, b int, c, d int, e, f int, g int could be replaced with a, b, c, d, e, f, g int
+/// func(a, b int, c, d int, e, f int, g int) could be replaced with func(a, b, c, d, e, f, g int)
 func mixedStyle5(a, b int, c, d int, e, f int, g int) {}
 
-/// a, b int, c int could be replaced with a, b, c int
+/// func() (a, b int, c int) could be replaced with func() (a, b, c int)
 func mixedStyle6() (a, b int, c int) { return 0, 0, 0 }
 
-/// a, b int, c, d int could be replaced with a, b, c, d int
+/// func() (a, b int, c, d int) could be replaced with func() (a, b, c, d int)
 func mixedStyle7() (a, b int, c, d int) { return 0, 0, 0, 0 }
 
-/// a int, b, c int could be replaced with a, b, c int
-/// d int, e int could be replaced with d, e int
+/// func(a int, b, c int) (d int, e int) could be replaced with func(a, b, c int) (d, e int)
 func mixedStyle8(a int, b, c int) (d int, e int) { return a, c }
 
-/// a int, b int, c int64, d int, e, f int64, _, g int64, h int, k int could be replaced with a, b int, c int64, d int, e, f, _, g int64, h, k int
+/// func(a int, b int, c int64, d int, e, f int64, _, g int64, h int, k int) could be replaced with func(a, b int, c int64, d int, e, f, _, g int64, h, k int)
 func mixedTypeWarn(a int, b int, c int64, d int, e, f int64, _, g int64, h int, k int) {}
--- lint/param-duplication_checker.go
+++ lint/param-duplication_checker.go
@@ -2,79 +2,65 @@ package lint
 
 import (
 	"go/ast"
-	"strings"
 
 	"github.com/Quasilyte/astcmp"
 )
 
 func paramDuplicationCheck(ctx *context) func(*ast.File) {
-	return wrapParamListChecker(&paramDuplicationChecker{
-		baseParamListChecker: baseParamListChecker{ctx: ctx},
+	return wrapFuncDeclChecker(&paramDuplicationChecker{
+		baseFuncDeclChecker: baseFuncDeclChecker{ctx: ctx},
 	})
 }
 
 type paramDuplicationChecker struct {
-	baseParamListChecker
-
-	cause *ast.FuncDecl
+	baseFuncDeclChecker
 }
 
-func (c *paramDuplicationChecker) PerFuncInit(fn *ast.FuncDecl) bool {
-	c.cause = fn
-	return true
-}
-
-func (c *paramDuplicationChecker) CheckParamList(params []*ast.Field) {
-	if len(params) < 2 {
-		return
+func (c *paramDuplicationChecker) CheckFuncDecl(decl *ast.FuncDecl) {
+	typ := c.optimizeFuncType(decl.Type)
+	if !astcmp.EqualExpr(typ, decl.Type) {
+		c.warn(decl.Type, typ)
 	}
-	equalRange := [][]*ast.Field{}
-	equalRange = append(equalRange, []*ast.Field{params[0]})
-	warn := false
-	for i, p := range params[1:] {
-		if astcmp.EqualExpr(p.Type, params[i].Type) {
-			equalRange[len(equalRange)-1] =
-				append(equalRange[len(equalRange)-1], p)
-			warn = true
+}
+
+func (c *paramDuplicationChecker) optimizeFuncType(f *ast.FuncType) *ast.FuncType {
+	return &ast.FuncType{
+		Params:  c.optimizeParams(f.Params),
+		Results: c.optimizeParams(f.Results),
+	}
+}
+func (c *paramDuplicationChecker) optimizeParams(params *ast.FieldList) *ast.FieldList {
+	if params == nil || len(params.List) < 2 {
+		return params
+	}
+
+	list := []*ast.Field{}
+	names := make([]*ast.Ident, len(params.List[0].Names))
+	copy(names, params.List[0].Names)
+	list = append(list, &ast.Field{
+		Names: names,
+		Type:  params.List[0].Type,
+	})
+	for i, p := range params.List[1:] {
+		names = make([]*ast.Ident, len(p.Names))
+		copy(names, p.Names)
+		if astcmp.EqualExpr(p.Type, params.List[i].Type) {
+			list[len(list)-1].Names =
+				append(list[len(list)-1].Names, names...)
 		} else {
-			equalRange = append(equalRange, []*ast.Field{p})
+			list = append(list, &ast.Field{
+				Names: names,
+				Type:  params.List[i+1].Type,
+			})
 		}
 	}
-	if warn {
-		c.warn(equalRange)
+	return &ast.FieldList{
+		List: list,
 	}
 }
 
-func (c *paramDuplicationChecker) warn(fields [][]*ast.Field) {
-	paramsBefore := []string{}
-	paramsAfter := []string{}
-	for _, flist := range fields {
-		typeBefore := []string{}
-		typeAfter := []string{}
-
-		for _, f := range flist {
-			typeBefore = append(typeBefore,
-				c.paramNamesStr(f.Names)+" "+nodeString(c.ctx.FileSet, f.Type))
-			typeAfter = append(typeAfter,
-				c.paramNamesStr(f.Names))
-		}
-
-		paramsBefore = append(paramsBefore, strings.Join(typeBefore, ", "))
-		paramsAfter = append(paramsAfter,
-			strings.Join(typeAfter, ", ")+" "+nodeString(c.ctx.FileSet, flist[0].Type))
-	}
-	c.ctx.Warn(c.cause, "%s could be replaced with %s",
-		strings.Join(paramsBefore, ", "),
-		strings.Join(paramsAfter, ", "))
-}
-
-func (c *paramDuplicationChecker) paramNamesStr(idents []*ast.Ident) string {
-	if idents == nil {
-		return "_"
-	}
-	names := []string{}
-	for _, id := range idents {
-		names = append(names, id.Name)
-	}
-	return strings.Join(names, ", ")
+func (c *paramDuplicationChecker) warn(f1, f2 *ast.FuncType) {
+	c.ctx.Warn(f1, "%s could be replaced with %s",
+		nodeString(c.ctx.FileSet, f1),
+		nodeString(c.ctx.FileSet, f2))
 }
