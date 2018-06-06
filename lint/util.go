fbfaa73b35771d2ee763a44969f500b5b6040383
Iskander Sharipov <iskander.sharipov@intel.com>
2018-06-07 00:24:20 +0300

    lint: use astutil.Unparen and remove unparen from util.go (#152)

--- lint/util.go
+++ lint/util.go
@@ -9,17 +9,6 @@ import (
 	"strings"
 )
 
-// unparen returns expr without surrounding parenthesis.
-// Only 1 level of ParenExpr is removed.
-// In other words, for ((x)) it returns (x),
-// second invocation will return just x.
-func unparen(expr ast.Expr) ast.Expr {
-	if expr, ok := expr.(*ast.ParenExpr); ok {
-		return expr.X
-	}
-	return expr
-}
-
 func nodeString(fset *token.FileSet, x ast.Node) string {
 	var buf bytes.Buffer
 	if err := printer.Fprint(&buf, fset, x); err != nil {
