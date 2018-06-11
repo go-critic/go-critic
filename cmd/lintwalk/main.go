8547c145f3af240b5a913921c258a891cca17dd3
Alekseev Artem <a.artem060@gmail.com>
2018-06-11 21:12:23 +0300

    fix lintwalk

--- cmd/lintwalk/main.go
+++ cmd/lintwalk/main.go
@@ -2,18 +2,43 @@ package lintwalk
 
 import (
 	"flag"
+	"go/build"
+	"go/parser"
+	"go/token"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"runtime"
+	"strings"
 )
+
+func packagePath() []string {
+	res := []string{runtime.GOROOT(), build.Default.GOPATH}
+
+	if p, ok := os.LookupEnv("GOPATH"); ok {
+		res = append(res, strings.Split(p, ":")...)
+	}
+	return res
+}
+
+func getPackagePrefix(dir string) string {
+	for _, p := range packagePath() {
+		if filepath.HasPrefix(dir, p) {
+			if res, err := filepath.Rel(filepath.Join(p, "src"), dir); err == nil {
+				return res
+			}
+		}
+	}
+	return ""
+}
 
 // Main implements gocritic sub-command entry point.
 func Main() {
 	enable := flag.String("enable", "all",
 		`forwarded to linter "as is"`)
-	exclude := flag.String("exclude", "testdata/|vendor/|builtin/",
+	exclude := flag.String("exclude", "(.*test.*)|(.*vendor.*)|(.*builtin.*)",
 		`regexp used to skip package names`)
 
 	flag.Parse()
@@ -27,39 +52,61 @@ func Main() {
 	}
 	srcRoot = filepath.Clean(srcRoot)
 
-	excludeRE, err := regexp.Compile(*exclude)
+	srcRoot, err := filepath.Abs(srcRoot)
+
 	if err != nil {
-		log.Fatalf("bad -exclude pattern: %v", err)
+		log.Fatal(err)
 	}
 
-	packages := make(map[string]bool)
+	excludeRE, err := regexp.Compile(*exclude)
+
+	testRE := regexp.MustCompile(".*_test")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fs := token.NewFileSet()
+
+	packages := map[string]bool{}
+
 	err = filepath.Walk(srcRoot, func(path string, info os.FileInfo, err error) error {
+		p, err := filepath.Abs(path)
 		if err != nil {
-			log.Printf("walk error: %v", err)
+			log.Fatal(err)
+		}
+		pkgs, err := parser.ParseDir(fs, p, nil, parser.PackageClauseOnly)
+
+		if !info.IsDir() || excludeRE.MatchString(p) {
 			return nil
 		}
-		if info.IsDir() {
-			return nil
+		p = filepath.Dir(p)
+		p = getPackagePrefix(p)
+
+		for _, pk := range pkgs {
+			if testRE.MatchString(pk.Name) {
+				continue
+			}
+			if pk.Name == "main" {
+				packages[p] = true
+			} else {
+				packages[filepath.Join(p, pk.Name)] = true
+			}
 		}
-		path = path[len(srcRoot)+len("/"):]
-		if filepath.Ext(path) != ".go" {
-			return nil
-		}
-		if excludeRE.MatchString(path) {
-			return nil
-		}
-		packages[filepath.Dir(path)] = true
 		return nil
 	})
+
 	if err != nil {
-		log.Fatalf("walk src-root: %v", err)
+		log.Fatal(err)
 	}
 
 	var args []string
 	args = append(args, "check-package", "-enable", *enable)
-	for pkg := range packages {
-		args = append(args, pkg)
+
+	for p := range packages {
+		args = append(args, p)
 	}
+
 	/* #nosec */
 	cmd := exec.Command("gocritic", args...)
 	cmd.Stdout = os.Stdout
