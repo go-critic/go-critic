b856c590bac84c43297a8303c6cfa3ff6f872058
Iskander Sharipov <quasilyte@gmail.com>
2018-06-21 18:48:52 +0300

    lint: check for all local defs in builtinShadow
    Changed warning text to reflect new semantics.
    Updated tests accordingly.
    
    Fixes #182

--- cmd/gocritic/testdata/builtinShadow/positive_tests.go
+++ cmd/gocritic/testdata/builtinShadow/positive_tests.go
@@ -1,163 +1,198 @@
 package checker_test
 
+/// shadowing of predeclared identifier: len
+/// shadowing of predeclared identifier: cap
+/// shadowing of predeclared identifier: complex64
+func warnParams(len int, cap string) (complex64 complex128) {
+	return 0
+}
+
+type shadower struct{}
+
+/// shadowing of predeclared identifier: bool
+func (bool shadower) shadowThem() {}
+
+func genDeclShadow() {
+	/// shadowing of predeclared identifier: byte
+	var byte = 1
+	var (
+		/// shadowing of predeclared identifier: true
+		true = false
+		/// shadowing of predeclared identifier: false
+		false = 10
+	)
+	/// shadowing of predeclared identifier: error
+	const error = "error"
+	const (
+		/// shadowing of predeclared identifier: float32
+		float32 = 32
+		/// shadowing of predeclared identifier: float64
+		float64 = 64
+	)
+
+	_ = byte
+	_ = true
+	_ = false
+}
+
 func assigningToBuiltinIdentifiers() {
 	// Types:
-	/// assigning to predeclared identifier: bool
+	/// shadowing of predeclared identifier: bool
 	bool := "1"
 	_ = bool
 
-	/// assigning to predeclared identifier: byte
+	/// shadowing of predeclared identifier: byte
 	byte := "1"
 	_ = byte
 
-	/// assigning to predeclared identifier: complex64
+	/// shadowing of predeclared identifier: complex64
 	complex64 := "1"
 	_ = complex64
 
-	/// assigning to predeclared identifier: complex128
+	/// shadowing of predeclared identifier: complex128
 	complex128 := "1"
 	_ = complex128
 
-	/// assigning to predeclared identifier: error
+	/// shadowing of predeclared identifier: error
 	error := "1"
 	_ = error
 
-	/// assigning to predeclared identifier: float32
+	/// shadowing of predeclared identifier: float32
 	float32 := "1"
 	_ = float32
 
-	/// assigning to predeclared identifier: float64
+	/// shadowing of predeclared identifier: float64
 	float64 := "1"
 	_ = float64
 
-	/// assigning to predeclared identifier: int
+	/// shadowing of predeclared identifier: int
 	int := "1"
 	_ = int
 
-	/// assigning to predeclared identifier: int8
+	/// shadowing of predeclared identifier: int8
 	int8 := "1"
 	_ = int8
 
-	/// assigning to predeclared identifier: int16
+	/// shadowing of predeclared identifier: int16
 	int16 := "1"
 	_ = int16
 
-	/// assigning to predeclared identifier: int32
+	/// shadowing of predeclared identifier: int32
 	int32 := "1"
 	_ = int32
 
-	/// assigning to predeclared identifier: int64
+	/// shadowing of predeclared identifier: int64
 	int64 := "1"
 	_ = int64
 
-	/// assigning to predeclared identifier: rune
+	/// shadowing of predeclared identifier: rune
 	rune := 10
 	_ = rune
 
-	/// assigning to predeclared identifier: string
+	/// shadowing of predeclared identifier: string
 	string := 10
 	_ = string
 
-	/// assigning to predeclared identifier: uint
+	/// shadowing of predeclared identifier: uint
 	uint := "1"
 	_ = uint
 
-	/// assigning to predeclared identifier: uint8
+	/// shadowing of predeclared identifier: uint8
 	uint8 := "1"
 	_ = uint8
 
-	/// assigning to predeclared identifier: uint16
+	/// shadowing of predeclared identifier: uint16
 	uint16 := "1"
 	_ = uint16
 
-	/// assigning to predeclared identifier: uint32
+	/// shadowing of predeclared identifier: uint32
 	uint32 := "1"
 	_ = uint32
 
-	/// assigning to predeclared identifier: uint64
+	/// shadowing of predeclared identifier: uint64
 	uint64 := "1"
 	_ = uint64
 
-	/// assigning to predeclared identifier: uintptr
+	/// shadowing of predeclared identifier: uintptr
 	uintptr := "1"
 	_ = uintptr
 
 	// Constants:
-	/// assigning to predeclared identifier: true
+	/// shadowing of predeclared identifier: true
 	true := "1"
 	_ = true
 
-	/// assigning to predeclared identifier: false
+	/// shadowing of predeclared identifier: false
 	false := "1"
 	_ = false
 
-	/// assigning to predeclared identifier: iota
+	/// shadowing of predeclared identifier: iota
 	iota := "1"
 	_ = iota
 
 	// Zero value:
-	/// assigning to predeclared identifier: nil
+	/// shadowing of predeclared identifier: nil
 	nil := "1"
 	_ = nil
 
 	// Functions:
-	/// assigning to predeclared identifier: append
+	/// shadowing of predeclared identifier: append
 	append := 1
 	_ = append
 
-	/// assigning to predeclared identifier: cap
+	/// shadowing of predeclared identifier: cap
 	cap := 1
 	_ = cap
 
-	/// assigning to predeclared identifier: close
+	/// shadowing of predeclared identifier: close
 	close := 1
 	_ = close
 
-	/// assigning to predeclared identifier: complex
+	/// shadowing of predeclared identifier: complex
 	complex := 1
 	_ = complex
 
-	/// assigning to predeclared identifier: copy
+	/// shadowing of predeclared identifier: copy
 	copy := 1
 	_ = copy
 
-	/// assigning to predeclared identifier: delete
+	/// shadowing of predeclared identifier: delete
 	delete := 1
 	_ = delete
 
-	/// assigning to predeclared identifier: imag
+	/// shadowing of predeclared identifier: imag
 	imag := 1
 	_ = imag
 
-	/// assigning to predeclared identifier: len
+	/// shadowing of predeclared identifier: len
 	len := 1
 	_ = len
 
-	/// assigning to predeclared identifier: make
+	/// shadowing of predeclared identifier: make
 	make := 1
 	_ = make
 
-	/// assigning to predeclared identifier: new
+	/// shadowing of predeclared identifier: new
 	new := 1
 	_ = new
 
-	/// assigning to predeclared identifier: panic
+	/// shadowing of predeclared identifier: panic
 	panic := 1
 	_ = panic
 
-	/// assigning to predeclared identifier: print
+	/// shadowing of predeclared identifier: print
 	print := 1
 	_ = print
 
-	/// assigning to predeclared identifier: println
+	/// shadowing of predeclared identifier: println
 	println := 1
 	_ = println
 
-	/// assigning to predeclared identifier: real
+	/// shadowing of predeclared identifier: real
 	real := 1
 	_ = real
 
-	/// assigning to predeclared identifier: recover
+	/// shadowing of predeclared identifier: recover
 	recover := 1
 	_ = recover
 }
