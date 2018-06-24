0efc4ada664da0873d1c8175118272a0e2bfbfb4
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-06-24 11:42:45 +0200

    lint/docStub: do not check unexported funcs, warn about XXX

--- cmd/gocritic/testdata/docStub/positive_tests.go
+++ cmd/gocritic/testdata/docStub/positive_tests.go
@@ -2,6 +2,12 @@ package checker_test
 
 // Foo ...
 /// silencing go lint doc-comment warnings is unadvised
-func Foo() {
+func Foo() {}
 
-}
+// Baz XXX.
+/// silencing go lint doc-comment warnings is unadvised
+func Baz() {}
+
+// Barr XXX
+/// silencing go lint doc-comment warnings is unadvised
+func Barr() {}
