6deac097a1200e5f374bd27c2281b7139528fb57
Iskander Sharipov <quasilyte@gmail.com>
2018-06-26 21:38:40 +0300

    remove begin&end tmp vars

--- lint/internal/astwalk/local_comment_visitor.go
+++ lint/internal/astwalk/local_comment_visitor.go
@@ -12,12 +12,10 @@ func (w *localCommentVisitor) WalkFile(f *ast.File) {
 		if !ok || !w.visitor.EnterFunc(decl) {
 			continue
 		}
-		begin := decl.Pos()
-		end := decl.End()
 		for _, c := range f.Comments {
 			// Not sure that decls/comments are sorted
 			// by positions, so do a naive full scan for now.
-			if c.Pos() < begin || c.Pos() > end {
+			if c.Pos() < decl.Pos() || c.Pos() > decl.End() {
 				continue
 			}
 			w.visitor.VisitLocalComment(c)
