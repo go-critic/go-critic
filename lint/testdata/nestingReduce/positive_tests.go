5f58c8acf055dd9b42c3836eab7a53cf56ff3022
Alekseev Artem <a.artem060@gmail.com>
2018-06-30 19:57:30 +0300

    fix tests

--- lint/testdata/nestingReduce/positive_tests.go
+++ lint/testdata/nestingReduce/positive_tests.go
@@ -1,7 +1,7 @@
 package checker_test
 
 func warnings1(a int) {
-	/// Nesting level could be reduced.
+	/// nesting level could be reduced
 	if a == 5 {
 		warnings1(a)
 		warnings1(a)
@@ -14,7 +14,7 @@ func warnings1(a int) {
 
 func warnings2(a []int) {
 	for _, v := range a {
-		/// Nesting level could be reduced.
+		/// nesting level could be reduced
 		if v == 5 {
 			warnings2(a)
 			warnings2(a)
