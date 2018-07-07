80526a2ef7369e7d6bc42c0f35fa795a47fb453a
Iskander Sharipov <quasilyte@gmail.com>
2018-07-07 13:16:48 +0300

    lint: add elseif checker
    Fixes #385

---
+++ lint/elseif_checker.go
@@ -0,0 +1,47 @@
+package lint
+
+//! Detects else with nested if statement that can be replaced with else-if.
+//
+// @Before:
+// if cond1 {
+// } else {
+// 	if x := cond2; x {
+// 	}
+// }
+//
+// @After:
+// if cond1 {
+// } else if x := cond2; x {
+// }
+
+import (
+	"go/ast"
+
+	"github.com/go-toolsmith/astp"
+)
+
+func init() {
+	// Opinionated because it does give questionable advices for cases
+	// where else with nested if is used for readability with preceding if body.
+	addChecker(&elseifChecker{}, attrExperimental, attrVeryOpinionated)
+}
+
+type elseifChecker struct {
+	checkerBase
+}
+
+func (c *elseifChecker) VisitStmt(stmt ast.Stmt) {
+	if stmt, ok := stmt.(*ast.IfStmt); ok {
+		elseBody, ok := stmt.Else.(*ast.BlockStmt)
+		if !ok || len(elseBody.List) != 1 {
+			return
+		}
+		if astp.IsIfStmt(elseBody.List[0]) {
+			c.warn(stmt.Else)
+		}
+	}
+}
+
+func (c *elseifChecker) warn(cause ast.Node) {
+	c.ctx.Warn(cause, "can replace 'else {if cond {}}' with 'else if cond {}'")
+}
