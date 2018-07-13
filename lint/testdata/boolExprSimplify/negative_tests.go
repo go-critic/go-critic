8c5643d29c6531fe8aab9f53549ae334c962a977
Iskander Sharipov <quasilyte@gmail.com>
2018-07-13 03:20:31 +0300

    lint: teach boolExprSimplify to recognize combined comparisons
    Added rules include:
    	x > y || x == y => x >= y
    	x < y || x == y => x <= y
    
    Plus their inverted lhs/rhs forms.

--- lint/testdata/boolExprSimplify/negative_tests.go
+++ lint/testdata/boolExprSimplify/negative_tests.go
@@ -16,3 +16,27 @@ func otherBinOps() {
 	_ = true && false
 	_ = true || false
 }
+
+func cantCombine() {
+	fn := func() int { return 0 }
+
+	var x, y, z int
+
+	// OK: not safe expressions.
+	_ = fn() > y || fn() == y
+	_ = fn() == y || fn() > y
+	_ = x > fn() || x == fn()
+	_ = x == fn() || x > fn()
+	_ = fn() > fn() || fn() == fn()
+	_ = fn() == fn() || fn() > fn()
+
+	// OK: different operands.
+	_ = x > y || x == z
+	_ = x == z || x > y
+	_ = x > z || x == y
+	_ = x == y || x > z
+
+	// OK: unrelated operations.
+	_ = x < y || x > z
+	_ = x > z || x < y
+}
