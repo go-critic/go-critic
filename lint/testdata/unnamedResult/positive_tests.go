2a0fe773688561405a1e1b83eba78b65f29aa642
mike stedman <barbeque@users.noreply.github.com>
2018-07-12 22:06:12 -0600

    Change 'consider to give name to results' to 'consider giving a name to this result'

--- lint/testdata/unnamedResult/positive_tests.go
+++ lint/testdata/unnamedResult/positive_tests.go
@@ -2,57 +2,57 @@ package checker_test
 
 type foo struct{}
 
-/// consider to give name to results
+/// consider giving a name to this result
 func (f *foo) f1() (float64, float64) {
 	return 0, 0
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func f2() (int, float64) {
 	return 0, 0
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func f3() (int, int, error) {
 	return 0, 0, nil
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func f4() (int, int, error) {
 	return 0, 0, nil
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func f5() (int, float32, bool) {
 	return 0, 0, false
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func f6() (bool, bool) {
 	return false, false
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func f7() (int, float32, *foo) {
 	return 0, 0, nil
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func (f *foo) f8() (bool, bool) {
 	return false, false
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func (f *foo) f9() (bool, func() int) {
 	return false, nil
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func f10() (int, int, float64, float64) {
 	return 0, 0, 0, 0
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func doubleError() (error, error) {
 	return nil, nil
 }
@@ -61,22 +61,22 @@ type namedInt int
 
 type namedStruct struct{}
 
-/// consider to give name to results
+/// consider giving a name to this result
 func named2ptr() (namedInt, *namedInt) {
 	return 0, nil
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func named2() (namedInt, namedInt) {
 	return 0, 0
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func named3() (namedInt, namedStruct, namedInt) {
 	return 0, namedStruct{}, 0
 }
 
-/// consider to give name to results
+/// consider giving a name to this result
 func namedAndPrimitive() (namedInt, int) {
 	return 0, 0
 }
