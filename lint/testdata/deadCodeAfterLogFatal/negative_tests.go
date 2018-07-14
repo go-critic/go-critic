8fdaa1e3ec1511c73414ebb322b7429aebe9f5e2
Lucas Bremgartner <lucas@bremis.ch>
2018-07-14 18:55:08 +0200

    lint: add returnAfterLogFatal checker (#444)
    Fixes #444

---
+++ lint/testdata/deadCodeAfterLogFatal/negative_tests.go
@@ -0,0 +1,23 @@
+package checker_tests
+
+import (
+	"log"
+)
+
+func noWarnings(x bool) {
+	if x {
+		log.Print()
+		log.Fatal()
+	}
+	log.Print()
+	log.Fatal()
+}
+
+func noWarningsWhenReturnInDifferentBlock(x bool) {
+	if x {
+		log.Print()
+		log.Fatal()
+	}
+	log.Print()
+	return
+}
