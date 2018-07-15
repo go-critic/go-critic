6e1b863a9379cf1598a3be9340c821013c41fd43
Andrey Velikoredchanin <andy@andyhost.ru>
2018-07-15 08:27:01 +0300

    lint: sqlRowsClose checker changes
    Change typical usage cases

--- lint/testdata/sqlRowsClose/positive_tests.go
+++ lint/testdata/sqlRowsClose/positive_tests.go
@@ -22,8 +22,26 @@ func normalPosLocalUse() {
 	return
 }
 
-/// param variable db.Rows have not Close call
-func testPosMethodCloseRows(rows *sql.Rows) {
+/// local variable db.Rows have not Close call
+func normalPosLocalUseWithCall() {
+	db, err := sql.Open("postgres", "")
+	if err != nil {
+		return
+	}
+
+	rows, err := db.Query("SELECT * FROM testtable")
+	if err != nil {
+		return
+	}
+
+	for rows.Next() {
+		testPosMethodCallRows(rows)
+	}
+
+	return
+}
+
+func testPosMethodCallRows(rows *sql.Rows) {
 	for rows.Next() {
 		var testdata string
 		rows.Scan(&testdata)
