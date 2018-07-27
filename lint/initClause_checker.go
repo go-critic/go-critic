ca2e1bb500fe04cb90f90348f5f217fb037a70a5
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-07-27 23:57:40 +0200

    all: initClause implementation (#501)

---
+++ lint/initClause_checker.go
@@ -0,0 +1,50 @@
+package lint
+
+import (
+	"go/ast"
+
+	"github.com/go-toolsmith/astp"
+)
+
+func init() {
+	addChecker(&initClauseChecker{}, attrSyntaxOnly, attrExperimental)
+}
+
+type initClauseChecker struct {
+	checkerBase
+}
+
+func (c *initClauseChecker) InitDocumentation(d *Documentation) {
+	d.Summary = "Detects non-assignment statements inside if/switch init clause."
+	d.Before = `if sideEffect(); cond {
+}`
+	d.After = `sideEffect()
+if cond {
+}`
+}
+
+func (c *initClauseChecker) VisitStmt(stmt ast.Stmt) {
+	initClause := c.getInitClause(stmt)
+	if initClause != nil && !astp.IsAssignStmt(initClause) {
+		c.warn(stmt, initClause)
+	}
+}
+
+func (c *initClauseChecker) getInitClause(x ast.Stmt) ast.Stmt {
+	switch x := x.(type) {
+	case *ast.IfStmt:
+		return x.Init
+	case *ast.SwitchStmt:
+		return x.Init
+	default:
+		return nil
+	}
+}
+
+func (c *initClauseChecker) warn(stmt, clause ast.Stmt) {
+	name := "if"
+	if astp.IsSwitchStmt(stmt) {
+		name = "switch"
+	}
+	c.ctx.Warn(stmt, "consider to move `%s` before %s", clause, name)
+}
