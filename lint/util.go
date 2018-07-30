8a0c91b33ee62c725e41d1933a9325f8c1be30ec
Iskander Sharipov <iskander.sharipov@intel.com>
2018-07-30 22:09:33 +0300

    lint: fix identOf function for selector expr (#533)
    Return the selected ident itself.

--- lint/util.go
+++ lint/util.go
@@ -64,16 +64,14 @@ func findNode(root ast.Node, pred func(ast.Node) bool) ast.Node {
 func identOf(x ast.Node) *ast.Ident {
 	switch x := x.(type) {
 	case *ast.Ident:
-		// Found ident.
 		return x
+	case *ast.SelectorExpr:
+		return identOf(x.Sel)
 	case *ast.TypeAssertExpr:
 		// x.(type) - x may contain ident.
 		return identOf(x.X)
 	case *ast.IndexExpr:
 		// x[i] - x may contain ident.
-		return identOf(x.X)
-	case *ast.SelectorExpr:
-		// x.y - x may contain ident.
 		return identOf(x.X)
 	case *ast.StarExpr:
 		// *x - x may contain ident.
