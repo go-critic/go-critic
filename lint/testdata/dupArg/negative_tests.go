64e744ff73b7a6c10cce0708c131a200c57c3e3e
Iskander (Alex) Sharipov <iskander.sharipov@intel.com>
2018-09-02 09:32:47 +0100

    lint: add dupArg checker (#604)
    Fixes #399

---
+++ lint/testdata/dupArg/negative_tests.go
@@ -0,0 +1,58 @@
+package checker_test
+
+import (
+	"bytes"
+	"go/types"
+	"image"
+	"image/draw"
+	"reflect"
+	"strings"
+)
+
+func differentArgs() {
+	var dstSlice, srcSlice []int
+	var s, s2 string
+	var b, b2 []byte
+	var dstRV, srcRV reflect.Value
+	var typ, typ2 types.Type
+	var dstImg, srcImg draw.Image
+
+	copy(dstSlice, srcSlice)
+
+	_ = reflect.Copy(dstRV, srcRV)
+	_ = reflect.DeepEqual(s, s2)
+
+	_ = strings.Contains(s, s2)
+	_ = strings.Compare(s, s2)
+	_ = strings.EqualFold(s, s2)
+	_ = strings.HasPrefix(s, s2)
+	_ = strings.HasSuffix(s, s2)
+	_ = strings.Index(s, s2)
+	_ = strings.LastIndex(s, s2)
+	_ = strings.Split(s, s2)
+	_ = strings.SplitAfter(s, s2)
+	_ = strings.SplitAfterN(s, s2, 2)
+	_ = strings.SplitN(s, s2, 2)
+
+	_ = bytes.Contains(b, b2)
+	_ = bytes.Compare(b, b2)
+	_ = bytes.Equal(b, b2)
+	_ = bytes.EqualFold(b, b2)
+	_ = bytes.HasPrefix(b, b2)
+	_ = bytes.HasSuffix(b, b2)
+	_ = bytes.LastIndex(b, b2)
+	_ = bytes.Split(b, b2)
+	_ = bytes.SplitAfter(b, b2)
+	_ = bytes.SplitAfterN(b, b2, 2)
+	_ = bytes.SplitN(b, b2, 2)
+
+	_ = types.Identical(typ, typ2)
+	_ = types.IdenticalIgnoreTags(typ, typ2)
+
+	{
+		var area image.Rectangle
+		var point image.Point
+		var op draw.Op
+		draw.Draw(dstImg, area, srcImg, point, op)
+	}
+}
