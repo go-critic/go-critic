df25b7dd605f62e6f56e6ad70eac54e0389e6b80
Anik Hasibul <25927971+AnikHasibul@users.noreply.github.com>
2018-09-07 01:59:00 +0600

    cmd: changed output formatting
    - Added colored output with -color flag (default true)
    - json output with -json gflag,
    - removed log prefix

--- cmd/criticize/main.go
+++ cmd/criticize/main.go
@@ -18,6 +18,7 @@ import (
 
 	"github.com/go-critic/go-critic/cmd/internal/flagparser"
 	"github.com/go-critic/go-critic/lint"
+	"github.com/logrusorgru/aurora"
 	"golang.org/x/tools/go/loader"
 )
 
@@ -293,7 +294,8 @@ func (l *linter) checkFile(f *ast.File) {
 				if l.flags.ShorterErrLocation {
 					loc = shortenLocation(loc)
 				}
-				log.Printf("%s: %s: %v\n", loc, c.Rule, warn.Text)
+
+				output(l, c.Rule.Name(), loc, warn.Text)
 			}
 		}(c)
 	}
@@ -309,4 +311,37 @@ func shortenLocation(loc string) string {
 	default:
 		return loc
 	}
+}
+
+func output(l *linter, rule, loc, warn string) {
+	switch {
+	case l.flags.JSONOutput:
+		// due to sort capabilities,
+		// using struct instead of map
+		b, err := json.MarshalIndent(struct {
+			Rule     string `json:"rule"`
+			Location string `json:"location"`
+			Warning  string `json:"warning"`
+		}{
+			Rule:     rule,
+			Location: loc,
+			Warning:  warn,
+		}, "", "    ")
+		if err != nil {
+			panic(err)
+		}
+		log.Println(string(b))
+	case l.flags.ColoredOutput:
+		log.Printf("%v:\n%v: %v\n",
+			aurora.Red(rule),
+			aurora.Magenta(
+				aurora.Bold(loc),
+			),
+			warn,
+		)
+
+	default:
+		log.Printf("%s:%s: %s\n", rule, loc, warn)
+	}
+
 }
