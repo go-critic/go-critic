d9a331cd6632bc7e1549a7714f1c594a4f65f19c
Iskander (Alex) Sharipov <iskander.sharipov@intel.com>
2018-10-28 23:13:57 +0300

    lint: simplify sloppyLen checker with lintpack.AsBinaryExpr (#702)
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- lint/sloppyLen_checker.go
+++ lint/sloppyLen_checker.go
@@ -4,6 +4,7 @@ import (
 	"go/ast"
 	"go/token"
 
+	"github.com/go-critic/go-critic/lint/internal/lintutil"
 	"github.com/go-toolsmith/astcopy"
 	"github.com/go-toolsmith/astfmt"
 )
@@ -28,11 +29,7 @@ len(arr) == 0`
 }
 
 func (c *sloppyLenChecker) VisitExpr(x ast.Expr) {
-	expr, ok := x.(*ast.BinaryExpr)
-	if !ok {
-		return
-	}
-
+	expr := lintutil.AsBinaryExpr(x)
 	if expr.Op == token.LSS || expr.Op == token.GEQ || expr.Op == token.LEQ {
 		if c.isLenCall(expr.X) && c.isZero(expr.Y) {
 			c.warn(expr)
