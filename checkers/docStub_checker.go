9f2768b5c984b5f47377fa9f76c731c223c56ee8
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2019-01-01 19:55:19 +0000

    checkers: make docStub more reliable (#747)
    Fixes #640
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- checkers/docStub_checker.go
+++ checkers/docStub_checker.go
@@ -2,7 +2,9 @@ package checkers
 
 import (
 	"go/ast"
+	"go/token"
 	"regexp"
+	"strings"
 
 	"github.com/go-lintpack/lintpack"
 	"github.com/go-lintpack/lintpack/astwalk"
@@ -25,9 +27,12 @@ func Foo() {}
 func Foo() {}`
 
 	collection.AddChecker(&info, func(ctx *lintpack.CheckerContext) lintpack.FileWalker {
-		c := &docStubChecker{ctx: ctx}
-		c.badCommentRE = regexp.MustCompile(`//\s?\w+([^a-zA-Z]+|( XXX.?))$`)
-		return astwalk.WalkerForFuncDecl(c)
+		re := `(?i)^\.\.\.$|^\.$|^xxx\.?$|^whatever\.?$`
+		c := &docStubChecker{
+			ctx:           ctx,
+			stubCommentRE: regexp.MustCompile(re),
+		}
+		return c
 	})
 }
 
@@ -35,15 +40,56 @@ type docStubChecker struct {
 	astwalk.WalkHandler
 	ctx *lintpack.CheckerContext
 
-	badCommentRE *regexp.Regexp
+	stubCommentRE *regexp.Regexp
 }
 
-func (c *docStubChecker) VisitFuncDecl(decl *ast.FuncDecl) {
-	if decl.Name.IsExported() && decl.Doc != nil && c.badCommentRE.MatchString(decl.Doc.List[0].Text) {
+func (c *docStubChecker) WalkFile(f *ast.File) {
+	for _, decl := range f.Decls {
+		switch decl := decl.(type) {
+		case *ast.FuncDecl:
+			c.visitDoc(decl, decl.Name, decl.Doc, false)
+		case *ast.GenDecl:
+			if decl.Tok != token.TYPE {
+				continue
+			}
+			if len(decl.Specs) == 1 {
+				spec := decl.Specs[0].(*ast.TypeSpec)
+				// Only 1 spec, use doc from the decl itself.
+				c.visitDoc(spec, spec.Name, decl.Doc, true)
+			}
+			// N specs, use per-spec doc.
+			for _, spec := range decl.Specs {
+				spec := spec.(*ast.TypeSpec)
+				c.visitDoc(spec, spec.Name, spec.Doc, true)
+			}
+		}
+	}
+}
+
+func (c *docStubChecker) visitDoc(decl ast.Node, sym *ast.Ident, doc *ast.CommentGroup, article bool) {
+	if !sym.IsExported() || doc == nil {
+		return
+	}
+	line := strings.TrimSpace(doc.List[0].Text[len("//"):])
+	if article {
+		// Skip optional article.
+		for _, a := range []string{"The ", "An ", "A "} {
+			if strings.HasPrefix(line, a) {
+				line = line[len(a):]
+				break
+			}
+		}
+	}
+	if !strings.HasPrefix(line, sym.Name) {
+		return
+	}
+	line = strings.TrimSpace(line[len(sym.Name):])
+	// Now try to detect the "stub" part.
+	if c.stubCommentRE.MatchString(line) {
 		c.warn(decl)
 	}
 }
 
-func (c *docStubChecker) warn(decl *ast.FuncDecl) {
-	c.ctx.Warn(decl, "silencing go lint doc-comment warnings is unadvised")
+func (c *docStubChecker) warn(cause ast.Node) {
+	c.ctx.Warn(cause, "silencing go lint doc-comment warnings is unadvised")
 }
--- checkers/testdata/docStub/positive_tests.go
+++ checkers/testdata/docStub/positive_tests.go
@@ -1,5 +1,24 @@
 package checker_test
 
+// Z .
+/*! silencing go lint doc-comment warnings is unadvised */
+func Z() {
+}
+
+type (
+	// An XX .
+	/*! silencing go lint doc-comment warnings is unadvised */
+	XX struct{}
+)
+
+// An YY whatever.
+/*! silencing go lint doc-comment warnings is unadvised */
+type YY struct{}
+
+// The ZZ whatever
+/*! silencing go lint doc-comment warnings is unadvised */
+type ZZ struct{}
+
 // Foo ...
 /*! silencing go lint doc-comment warnings is unadvised */
 func Foo() {}
