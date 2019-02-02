34c1dc890d7dcb9ce16a2e3f3d376f2a9061913b
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2019-02-02 16:29:08 +0300

    checkers: add Split handling to argOrder checker (#791)
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- checkers/testdata/argOrder/negative_tests.go
+++ checkers/testdata/argOrder/negative_tests.go
@@ -14,10 +14,30 @@ func nonConstArgs(s1, s2 string, b1, b2 []byte) {
 	_ = bytes.HasPrefix([]byte(s1), b1)
 }
 
+func constOnlyArgs() {
+	_ = strings.HasPrefix("", "http://")
+	_ = bytes.HasPrefix([]byte{}, []byte("http://"))
+	_ = bytes.HasPrefix([]byte{}, []byte{'h', 't', 't', 'p', ':', '/', '/'})
+	_ = strings.Contains("", ":")
+	_ = bytes.Contains([]byte{}, []byte(":"))
+	_ = strings.TrimPrefix("", ":")
+	_ = bytes.TrimPrefix([]byte{}, []byte(":"))
+	_ = strings.TrimSuffix("", ":")
+	_ = bytes.TrimSuffix([]byte{}, []byte(":"))
+	_ = strings.Split("", "/")
+	_ = bytes.Split([]byte{}, []byte("/"))
+}
+
 func properArgsOrder(s string, b []byte) {
 	_ = strings.HasPrefix(s, "http://")
 	_ = bytes.HasPrefix(b, []byte("http://"))
 	_ = bytes.HasPrefix(b, []byte{'h', 't', 't', 'p', ':', '/', '/'})
 	_ = strings.Contains(s, ":")
 	_ = bytes.Contains(b, []byte(":"))
+	_ = strings.TrimPrefix(s, ":")
+	_ = bytes.TrimPrefix(b, []byte(":"))
+	_ = strings.TrimSuffix(s, ":")
+	_ = bytes.TrimSuffix(b, []byte(":"))
+	_ = strings.Split(s, "/")
+	_ = bytes.Split(b, []byte("/"))
 }
