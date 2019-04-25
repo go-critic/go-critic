8fb16e06b0b6ac24651c7aed15f1b9ba99d51cec
Iskander Sharipov <quasilyte@gmail.com>
2019-04-25 22:51:51 +0300

    checkers: add integration tests with cgo for dupImports
    Updates #845
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ checkers/testdata/_integration/cgo/main.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+void myprint(char* s) {
+	printf("%d\n", s);
+}
+*/
+import "C"
+
+import (
+	"unsafe"
+)
+
+func main() {
+	cs := C.CString("Hello from stdio\n")
+	C.myprint(cs)
+	C.free(unsafe.Pointer(cs))
+}
