70774a564e9aef39639d21662e689fdf348f52b1
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2019-10-25 15:14:33 +0300

    checkers: add truncateCmp checker (#878)
    Checks when numeric types are compared with truncated conversion on
    the either side.
    
    Problematic code example:
    ```
    	func f(x int32, y int16) bool {
    	  return int16(x) < y
    	}
    ```
    
    Fixed code example:
    ```
    	func f(x int32, int16) bool {
    	  return x < int32(y)
    	}
    ```
    
    See https://github.com/golang/go/issues/35010
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ checkers/testdata/truncateCmp/negative_tests.go
@@ -0,0 +1,68 @@
+package checker_test
+
+func cmpWithConst(x int64) {
+	_ = int16(x) < 0
+	_ = 0 < int16(x)
+}
+
+func sameSizes(x, y int16) {
+	_ = int16(y) < x
+	_ = x < int16(y)
+
+	_ = y < x
+	_ = x < y
+}
+
+func mixedSignedness1(x uint8, y int16) {
+	_ = uint8(y) < x
+	_ = x < uint8(y)
+}
+
+func mixedSignedness2(x int8, y uint16) {
+	_ = int8(y) < x
+	_ = x < int8(y)
+}
+
+func goodInt8(x int8, y int16) {
+	_ = y < int16(x)
+	_ = int16(x) < y
+
+	_ = y <= int16(x)
+	_ = int16(x) <= y
+
+	_ = y > int16(x)
+	_ = int16(x) > y
+
+	_ = y >= int16(x)
+	_ = int16(x) >= y
+
+	_ = y == int16(x)
+	_ = int16(x) == y
+
+	_ = y != int16(x)
+	_ = int16(x) != y
+}
+
+func goodInt16(x1 int8, x2 int16, y int32) {
+	_ = y == int32(x1)
+	_ = int32(x1) == y
+
+	_ = y == int32(x2)
+	_ = int32(x2) == y
+}
+
+func goodInt32(x1 int8, x2 int16, x3 int32, y int64) {
+	_ = y == int64(x1)
+	_ = int64(x1) == y
+
+	_ = y == int64(x2)
+	_ = int64(x2) == y
+
+	_ = y == int64(x3)
+	_ = int64(3) == y
+}
+
+func goodUint8(x uint8, y uint16) {
+	_ = y == uint16(x)
+	_ = uint16(x) == y
+}
