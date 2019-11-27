a2220958fdc481dd6e219e95b58375fa04ec9d92
Luke Meyers <snargleplax@users.noreply.github.com>
2019-11-27 03:02:15 -0800

    Add whyNoLint checker. (#887)
    * Add whyNoLint checker.
    
    This checker inspects machine-readable //nolint comments (per the
    conventions from https://github.com/golangci/golangci-lint#nolint) to
    ensure that they are accompanied by human-readable explanatory
    comments on the same line.
    
    The intention is to enable teams to enforce a policy of not disabling
    linters without providing an explicit justification.
    
    * Add backticks around code literal in Summary.
    
    * Factor out slice element to conditional-scoped var

---
+++ checkers/testdata/whyNoLint/negative_tests.go
@@ -0,0 +1,13 @@
+package checker_test
+
+/* canonical forms */
+//nolint // reason
+//nolint:gocritic // reason
+//nolint:gocritic,whyNoLint // reason
+
+/* variants we're okay enough with not to warn on; some other checker be strict about spacing */
+//nolint //reason
+//nolint//reason
+// nolint // reason
+
+//yeslint
---
+++ checkers/testdata/whyNoLint/positive_tests.go
@@ -0,0 +1,19 @@
+package checker_test
+
+/*! include an explanation for nolint directive */
+//nolint
+
+/*! include an explanation for nolint directive */
+//nolint:gocritic
+
+/*! include an explanation for nolint directive */
+//nolint:gocritic,whyNoLint
+
+/*! include an explanation for nolint directive */
+// nolint
+
+/*! include an explanation for nolint directive */
+//nolint nonsense
+
+/*! include an explanation for nolint directive */
+//nolint //
