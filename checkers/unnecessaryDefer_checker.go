571443405f5c7ce01e83f1403838198b83ab9c69
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2020-01-20 22:05:06 +0100

    checkers: rename deferAtTheEnd to unnecessaryDefer (#902)
    Also used lintutil.ContainsNode to remove containsCallExpr func.
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- checkers/unnecessaryDefer_checker.go
+++ checkers/unnecessaryDefer_checker.go
@@ -3,16 +3,18 @@ package checkers
 import (
 	"go/ast"
 
+	"github.com/go-critic/go-critic/checkers/internal/lintutil"
 	"github.com/go-lintpack/lintpack"
 	"github.com/go-lintpack/lintpack/astwalk"
 	"github.com/go-toolsmith/astfmt"
+	"github.com/go-toolsmith/astp"
 )
 
 func init() {
 	var info lintpack.CheckerInfo
-	info.Name = "deferAtTheEnd"
+	info.Name = "unnecessaryDefer"
 	info.Tags = []string{"diagnostic", "experimental"}
-	info.Summary = "Detects calls to defer at the end of function"
+	info.Summary = "Detects redundantly deferred calls"
 	info.Before = `
 func() {
 	defer os.Remove(filename)
@@ -23,16 +25,16 @@ func() {
 }`
 
 	collection.AddChecker(&info, func(ctx *lintpack.CheckerContext) lintpack.FileWalker {
-		return astwalk.WalkerForFuncDecl(&deferAtTheEnd{ctx: ctx})
+		return astwalk.WalkerForFuncDecl(&unnecessaryDeferChecker{ctx: ctx})
 	})
 }
 
-type deferAtTheEnd struct {
+type unnecessaryDeferChecker struct {
 	astwalk.WalkHandler
 	ctx *lintpack.CheckerContext
 }
 
-func (c *deferAtTheEnd) VisitFuncDecl(funcDecl *ast.FuncDecl) {
+func (c *unnecessaryDeferChecker) VisitFuncDecl(funcDecl *ast.FuncDecl) {
 	ast.Inspect(funcDecl.Body, func(n ast.Node) bool {
 		funDecl, ok := n.(*ast.BlockStmt)
 		if ok {
@@ -42,14 +44,14 @@ func (c *deferAtTheEnd) VisitFuncDecl(funcDecl *ast.FuncDecl) {
 	})
 }
 
-func (c *deferAtTheEnd) checkDeferBeforeReturn(funcDecl *ast.BlockStmt) {
+func (c *unnecessaryDeferChecker) checkDeferBeforeReturn(funcDecl *ast.BlockStmt) {
 	retIndex := len(funcDecl.List)
 	for i, stmt := range funcDecl.List {
 		retStmt, ok := stmt.(*ast.ReturnStmt)
 		if !ok {
 			continue
 		}
-		if containsCallExpr(retStmt) {
+		if lintutil.ContainsNode(retStmt, astp.IsCallExpr) {
 			continue
 		}
 		retIndex = i
@@ -65,16 +67,7 @@ func (c *deferAtTheEnd) checkDeferBeforeReturn(funcDecl *ast.BlockStmt) {
 	}
 }
 
-func containsCallExpr(retStmt *ast.ReturnStmt) bool {
-	for _, expr := range retStmt.Results {
-		if _, ok := expr.(*ast.CallExpr); ok {
-			return true
-		}
-	}
-	return false
-}
-
-func (c *deferAtTheEnd) warn(deferStmt *ast.DeferStmt) {
+func (c *unnecessaryDeferChecker) warn(deferStmt *ast.DeferStmt) {
 	s := astfmt.Sprint(deferStmt)
 	if fnlit, ok := deferStmt.Call.Fun.(*ast.FuncLit); ok {
 		// To avoid long and multi-line warning messages,
