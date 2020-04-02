b66aec0f45b5068357af39131d2c919875cf2383
Iskander Sharipov <quasilyte@gmail.com>
2020-04-03 00:14:05 +0300

    checkers: add regexpSimplify
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- checkers/checkers.go
+++ checkers/checkers.go
@@ -2,9 +2,18 @@
 package checkers
 
 import (
+	"os"
+
 	"github.com/go-lintpack/lintpack"
 )
 
 var collection = &lintpack.CheckerCollection{
 	URL: "https://github.com/go-critic/go-critic/checkers",
 }
+
+var debug = func() func() bool {
+	v := os.Getenv("DEBUG") != ""
+	return func() bool {
+		return v
+	}
+}()
