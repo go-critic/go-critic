52647af71f5000d1af12a8d39796cd6128443507
Iskander Sharipov <quasilyte@gmail.com>
2020-05-27 00:35:40 +0300

    checkers: add sqlQuery checker
    Reports `_, err := db.Query()` pattern.
    Suggests to use Exec() instead of Query in this case.
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ checkers/testdata/sqlQuery/negative_tests.go
@@ -0,0 +1,32 @@
+package checker_test
+
+import (
+	"database/sql"
+)
+
+func queryResultIsUsed(db *sql.DB, qe QueryExecer, mydb *myDatabase) {
+	const queryString = "SELECT * FROM users"
+
+	rows1, err := db.Query(queryString)
+	_ = rows1
+
+	rows2, err := qe.Query(queryString)
+	_ = rows2
+
+	rows3, err := mydb.Query(queryString)
+	_ = rows3
+
+	_ = err
+}
+
+func execIsUsed(db *sql.DB, qe QueryExecer, mydb *myDatabase) {
+	const queryString = "UPDATE users SET name = 'gopher'"
+
+	var err error
+
+	_, err = db.Exec(queryString)
+	_, err = qe.Exec(queryString)
+	_, err = mydb.Exec(queryString)
+
+	_ = err
+}
