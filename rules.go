ef3abb5b1b773432ad88ef9064b3e094d113b69d
Oleg Kovalov <iamolegkovalov@gmail.com>
2020-08-16 18:25:03 +0200

    review fix

--- rules.go
+++ rules.go
@@ -1,10 +1,10 @@
+//nolint
 package gorules
 
 import (
 	"github.com/quasilyte/go-ruleguard/dsl/fluent"
 )
 
-//nolint:deadcode,unused
 func nilSafeTypeOf(m fluent.Matcher) {
 	m.Match(`$_.ctx.TypesInfo.TypeOf($x)`).
 		Report(`use ctx.TypeOf($x) instead, it's nil-safe`)
