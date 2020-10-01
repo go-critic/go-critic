d01c12d18a5793012b2ef87e389e787f5baadf5b
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2020-10-02 01:43:40 +0300

    checkers: fix unlambda panic on variadic func literals (#992)
    Fixes #991
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- checkers/testdata/unlambda/positive_tests.go
+++ checkers/testdata/unlambda/positive_tests.go
@@ -22,6 +22,36 @@ func functionLiterals() {
 	_ = func(x int, y int) int { return add(x, y) }
 }
 
+func variadicInt(xs ...int) int { return 0 }
+
+func variadicTest() {
+	_ = func(x int) int { return variadicInt(x) }
+	_ = func(x int) int { return variadicInt(x, 1) }
+	_ = func(x, y int) int { return variadicInt(x, y) }
+	_ = func(x, y int) int { return variadicInt(x) }
+
+	/*! replace `func(xs ...int) int { return variadicInt(xs...) }` with `variadicInt` */
+	_ = func(xs ...int) int { return variadicInt(xs...) }
+
+	_ = func(x int, ys ...int) int { return variadicInt(1, 2) }
+	_ = func(x int, y int, _ ...int) int { return variadicInt(x, y) }
+}
+
+func variadicInterfaces(x int, y interface{}, ys ...interface{}) int { return 0 }
+
+func TestSomething() {
+	// See #991
+	_ = func(x int, y interface{}, _ ...interface{}) int {
+		return variadicInterfaces(x, y)
+	}
+	_ = func(x int, y interface{}, _ ...interface{}) int {
+		return variadicInterfaces(x, y, 5, "?")
+	}
+
+	/*! replace `func(x int, y interface{}, zs ...interface{}) int { return variadicInterfaces(x, y, zs...) }` with `variadicInterfaces` */
+	_ = func(x int, y interface{}, zs ...interface{}) int { return variadicInterfaces(x, y, zs...) }
+}
+
 type object struct{}
 
 func (object) returnInt(x int) int { return x }
