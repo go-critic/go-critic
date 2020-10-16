c16764c05ad271421b2dc6541b9ad2231c672350
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2020-10-16 14:21:09 +0200

    checkers: add badLock checker (#999)
    Finds mismatching Lock+Unlock sequences.
    
    	Lock+RUnlock -> error
    	RLock+Unlock -> error
    	Lock+Unlock -> OK
    	RLock+RUnlock -> OK
    
    It only checks the 2 adjusted statements.
    
    We don't check for types being sync.Mutex or sync.RWMutex,
    let's see how bad it will be for a false positive rates.
    People could embed locks or implement custom Lock+Unlock methods.
    We won't mark this checker as stable until it's properly tested,
    so it shouldn't be a deal breaker.
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ checkers/testdata/badLock/negative_tests.go
@@ -0,0 +1,39 @@
+package checker_test
+
+import (
+	"sync"
+)
+
+func deferredUnlock(mu *sync.Mutex, op func()) {
+	mu.Lock()
+	defer mu.Unlock()
+	op()
+}
+
+func goodUnlock(mu *sync.RWMutex, op func()) {
+	mu.Lock()
+	defer mu.Unlock()
+	op()
+}
+
+func goodRUnlock(mu *sync.RWMutex, op func()) {
+	mu.RLock()
+	defer mu.RUnlock()
+	op()
+}
+
+func differentMutexes(mu1, mu2 *sync.RWMutex, op func()) {
+	{
+		mu2.RLock()
+		mu1.Lock()
+		mu2.RUnlock()
+		mu1.Unlock()
+	}
+
+	{
+		mu1.Lock()
+		mu2.Lock()
+		mu1.Unlock()
+		mu2.Unlock()
+	}
+}
