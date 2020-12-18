476829876d00bcc1c2e29578c9caa400311578f9
Sebastien Rosset (serosset) <serosset@cisco.com>
2020-12-18 08:07:22 -0800

    Add support for glob pattern in ruleguard configuration

--- checkers/ruleguard_checker.go
+++ checkers/ruleguard_checker.go
@@ -22,7 +22,7 @@ func init() {
 	info.Params = linter.CheckerParams{
 		"rules": {
 			Value: "",
-			Usage: "comma-separated list of gorule file paths",
+			Usage: "comma-separated list of gorule file paths. Patterns such as 'rules-*.go' may be specified",
 		},
 		"debug": {
 			Value: "",
