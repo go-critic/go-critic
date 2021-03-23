b7c79ac71e4883904c34ffab3e5c3dc0405f3598
Sebastien Rosset (serosset) <serosset@cisco.com>
2021-03-23 10:16:11 -0700

    Add more integration tests

--- checkers/testdata/_integration/ruleguard-skip-import-error/rules1.go
+++ checkers/testdata/_integration/ruleguard-skip-import-error/rules1.go
@@ -6,12 +6,12 @@ import "github.com/quasilyte/go-ruleguard/dsl"
 
 func badLock(m dsl.Matcher) {
 	m.Import("foo.bar")
-	m.Match(`$mu.Lock(); defer $mu.RUnlock()`).Report(`maybe $mu.RLock() was intended?`)
 	// DSL invalid predicate is intentional. This is for test purpose.
 	// This is specifically used to test the '-failOnError' flag when a
 	// DSL syntax error or import error is encountered while parsing ruleguard files.
 	// Use case: a ruleguard file contains an import that may or may not exist depending
 	// on the module being analyzed.
 	m.Match(`$mu.RLock(); defer $mu.Unlock()`).
+		Where(m["mu].Type.Implements("foo.bar.MyInterface")).
 		Report(`maybe $mu.Lock() was intended?`)
 }
