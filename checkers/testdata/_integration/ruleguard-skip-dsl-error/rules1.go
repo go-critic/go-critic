cd1a428f989584c455360455f4a0f951199a004c
Sebastien Rosset (serosset) <serosset@cisco.com>
2021-03-23 09:23:05 -0700

    Add more integration tests

--- checkers/testdata/_integration/ruleguard-skip-dsl-error/rules1.go
+++ checkers/testdata/_integration/ruleguard-skip-dsl-error/rules1.go
@@ -9,13 +9,11 @@ func badLock(m dsl.Matcher) {
 	// DSL invalid predicate is intentional. This is for test purpose.
 	// In this test, -@ruleguard.failOnError=dsl is NOT specified, so go-critic should
 	// ignore this file.
-	// This is specifically used to test the '-failOnError' flag when a
-	// DSL syntax error or import error is encountered while parsing ruleguard files.
-	// Use case 1: suppose a directory contains multiple ruleguard rules.
+	// I.e. skip files that contain a DSL syntax error.
+	//
+	// Use case: suppose a directory contains multiple ruleguard rules.
 	// One of these files contains a ruleguard DSL with a newer keyword which is not supported
 	// by the installed version of go-critic + ruleguard.
-	// Use case 2: a ruleguard file contains an import that may or may not exist depending
-	// on the module being analyzed.
 	m.Match(`$mu.RLock(); defer $mu.Unlock()`).
 		Where(version == "1.17").
 		Report(`maybe $mu.Lock() was intended?`)
