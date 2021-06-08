f9f09a8f9988dee3976871571aee63724c7cef62
Sebastien Rosset (serosset) <serosset@cisco.com>
2021-06-08 02:25:31 +0000

    remove integration tests, not working unless it is the upstream repo

--- checkers/testdata/_integration/ruleguard-skip-dsl-error/rules2.go
+++
@@ -1,9 +0,0 @@
-// +build ignore
-
-package gorules
-
-import "github.com/quasilyte/go-ruleguard/dsl"
-
-func badLock(m dsl.Matcher) {
-	m.Match(`$mu.Lock(); defer $mu.RUnlock()`).Report(`maybe $mu.RLock() was intended?`)
-}
