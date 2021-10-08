17fdc33732abf62c542a98512950d4fc457cdd28
peakle <denislimarev@gmail.com>
2021-10-08 23:33:37 +0300

    checkers: add test cases for singleCaseSwitch checker (#1071)

--- checkers/testdata/singleCaseSwitch/negative_tests.go
+++ checkers/testdata/singleCaseSwitch/negative_tests.go
@@ -28,6 +28,19 @@ func caseWithTwoValues(x int) {
 	switch x {
 	case 1, 2:
 	}
+
+	switch xx := interface{}(x).(type) {
+	case int, uint:
+		println(xx)
+	}
+
+	switch {
+	case false, true:
+	}
+
+	switch 1 {
+	case 2, 3:
+	}
 }
 
 func caseWithBreak(x interface{}) {
@@ -43,10 +56,95 @@ func caseWithBreak(x interface{}) {
 		break
 	}
 
+	switch xx := x.(type) {
+	default:
+		println(xx)
+		break
+	}
+
 	for {
 		switch x.(int) {
 		case 0:
 			println(x)
+			break
+		}
+	}
+}
+
+func caseWithSelect(ch chan string) {
+	switch <-ch {
+	case "1", "2":
+		select {
+		case ch <- "1":
+		}
+	}
+
+	switch <-ch {
+	case "1":
+		select {
+		case ch <- "1":
+		}
+	default:
+		select {
+		default:
+		}
+	}
+
+	switch x := interface{}(<-ch).(type) {
+	case int, uint:
+		select {
+		case ch <- "1":
+			println(x)
+		}
+	}
+
+	select {
+	case <-ch:
+		switch <-ch {
+		default:
+		case "1":
+		}
+	}
+
+	select {
+	default:
+		switch <-ch {
+		case "1", "2":
+		}
+	}
+}
+
+func caseWithRange(a []string) {
+	switch true {
+	case true:
+		for range a {
+		}
+	default:
+	}
+
+	switch x := interface{}(a).(type) {
+	case uint, int:
+		for range a {
+			println(x)
+		}
+	}
+
+	switch a[0] {
+	case "2", "3":
+		for range a {
+			println(a)
+		}
+	}
+
+	for i := range a {
+		switch i {
+		case 1, 2:
+		}
+	}
+
+	for i := range a {
+		switch i {
+		default:
 			break
 		}
 	}
--- checkers/testdata/singleCaseSwitch/positive_tests.go
+++ checkers/testdata/singleCaseSwitch/positive_tests.go
@@ -1,14 +1,22 @@
 package checker_test
 
-// TODO: add tests for "no warnings" cases.
-// TODO: add tests for expression switch statement.
-
 func intValue(x interface{}) int {
 	/*! should rewrite switch statement to if statement */
 	switch x := x.(type) {
 	case int:
 		return x
 	}
+
+	/*! should rewrite switch statement to if statement */
+	switch x := x.(type) {
+	case int:
+		/*! should rewrite switch statement to if statement */
+		switch x := interface{}(x).(type) {
+		case int:
+			println(x)
+		}
+	}
+
 	return 0
 }
 
@@ -17,12 +25,87 @@ func switchDefault(x interface{}) {
 	switch x.(type) {
 	default:
 	}
+
+	/*! found switch with default case only */
+	switch {
+	default:
+	}
+
+	/*! found switch with default case only */
+	switch {
+	default:
+		println(x)
+		for {
+			break
+		}
+	}
+
+	/*! found switch with default case only */
+	switch x := x.(type) {
+	default:
+		/*! should rewrite switch statement to if statement */
+		switch x {
+		case 1:
+		}
+	}
+
+	/*! found switch with default case only */
+	switch x := x.(type) {
+	default:
+		/*! should rewrite switch statement to if statement */
+		switch d := x.(type) {
+		case int:
+			println(d)
+		}
+	}
+
+	/*! found switch with default case only */
+	switch x := x.(type) {
+	default:
+		switch x {
+		case 1:
+		case 2:
+		}
+	}
 }
 
 func switchWithOneCase(x int) {
 	/*! should rewrite switch statement to if statement */
 	switch x {
 	case 1:
+	}
+
+	/*! should rewrite switch statement to if statement */
+	switch {
+	case true:
+	}
+
+	/*! should rewrite switch statement to if statement */
+	switch x == 1 {
+	case true:
+	}
+
+	/*! should rewrite switch statement to if statement */
+	switch interface{}(x).(type) {
+	case int32:
+	}
+
+	/*! should rewrite switch statement to if statement */
+	switch x {
+	case 1:
+		/*! should rewrite switch statement to if statement */
+		switch x {
+		case 2:
+		}
+	}
+
+	/*! should rewrite switch statement to if statement */
+	switch x {
+	case 1:
+		switch x {
+		case 2:
+		case 3:
+		}
 	}
 }
 
@@ -43,6 +126,121 @@ func badCaseWithBreak(x, y int) {
 		switch y {
 		case 2:
 			break
+		}
+	}
+
+	/*! should rewrite switch statement to if statement */
+	switch x {
+	case 0:
+		println(x)
+		switch y {
+		case 2:
+			break
+		case 1:
+			break
+		}
+	}
+}
+
+func badCaseWithRange(a []string) {
+	/*! should rewrite switch statement to if statement */
+	switch true {
+	case true:
+		for range a {
+		}
+	}
+
+	/*! should rewrite switch statement to if statement */
+	switch x := interface{}(a).(type) {
+	case uint:
+		for range a {
+			println(x)
+		}
+	}
+
+	/*! should rewrite switch statement to if statement */
+	switch a[0] {
+	case "2":
+		for range a {
+			println(a)
+		}
+	}
+
+	for i := range a {
+		/*! should rewrite switch statement to if statement */
+		switch i {
+		case 1:
+		}
+	}
+
+	for i := range a {
+		/*! found switch with default case only */
+		switch i {
+		default:
+		}
+	}
+}
+
+func badCaseWithSelect(ch chan string) {
+	/*! should rewrite switch statement to if statement */
+	switch {
+	case true:
+		select {
+		case <-ch:
+		}
+	}
+
+	/*! should rewrite switch statement to if statement */
+	switch x := interface{}(0).(type) {
+	case string:
+		select {
+		case ch <- x:
+		}
+	}
+
+	/*! should rewrite switch statement to if statement */
+	switch <-ch {
+	case "1":
+		select {
+		case ch <- "1":
+		}
+	}
+
+	/*! found switch with default case only */
+	switch {
+	default:
+		select {
+		default:
+		}
+	}
+
+	select {
+	case <-ch:
+		/*! found switch with default case only */
+		switch {
+		default:
+		}
+	default:
+		/*! should rewrite switch statement to if statement */
+		switch {
+		case true:
+		}
+	}
+
+	select {
+	default:
+		/*! should rewrite switch statement to if statement */
+		switch xx := interface{}(ch).(type) {
+		case int:
+			println(xx)
+		}
+	}
+
+	select {
+	case ch <- "2":
+		/*! should rewrite switch statement to if statement */
+		switch <-ch {
+		case "1":
 		}
 	}
 }
