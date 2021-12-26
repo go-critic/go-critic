b38e2eeb08794bd38f5088ada0e1f842367955df
Iskander Sharipov <quasilyte@gmail.com>
2021-12-26 15:00:16 +0300

    checkers: add tests for #1166

--- checkers/testdata/commentFormatting/positive_tests.go
+++ checkers/testdata/commentFormatting/positive_tests.go
@@ -6,6 +6,12 @@ package checker_test
 /*! put a space between `//` and comment text */
 //nolinto
 
+/*! put a space between `//` and comment text */
+//no inspection
+
+/*! put a space between `//` and comment text */
+//no inspection something
+
 func f1() {
 	/*! put a space between `//` and comment text */
 	//block with
