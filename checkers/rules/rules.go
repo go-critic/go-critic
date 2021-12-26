785d1f64843fff39399c7c1d0a6cabf09afa2382
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2021-12-26 15:18:47 +0300

    update ruleguard (#1173)

--- checkers/rules/rules.go
+++ checkers/rules/rules.go
@@ -636,15 +636,17 @@ func stringConcatSimplify(m dsl.Matcher) {
 //doc:before  t.Unix() / 1000
 //doc:after   t.UnixMilli()
 func timeExprSimplify(m dsl.Matcher) {
+	isTime := func(v dsl.Var) bool {
+		return v.Type.Is(`time.Time`) || v.Type.Is(`*time.Time`)
+	}
+
 	m.Match(`$t.Unix() / 1000`).
-		Where(m.GoVersion().GreaterEqThan("1.17") &&
-			(m["t"].Type.Is(`time.Time`) || m["t"].Type.Is(`*time.Time`))).
+		Where(m.GoVersion().GreaterEqThan("1.17") && isTime(m["t"])).
 		Suggest("$t.UnixMilli()").
 		Report(`use $t.UnixMilli() instead of $$`)
 
 	m.Match(`$t.UnixNano() * 1000`).
-		Where(m.GoVersion().GreaterEqThan("1.17") &&
-			(m["t"].Type.Is(`time.Time`) || m["t"].Type.Is(`*time.Time`))).
+		Where(m.GoVersion().GreaterEqThan("1.17") && isTime(m["t"])).
 		Suggest("$t.UnixMicro()").
 		Report(`use $t.UnixMicro() instead of $$`)
 }
@@ -654,20 +656,24 @@ func timeExprSimplify(m dsl.Matcher) {
 //doc:before  type Foo struct{ ...; sync.Mutex; ... }
 //doc:after   type Foo struct{ ...; mu sync.Mutex; ... }
 func exposedSyncMutex(m dsl.Matcher) {
+	isExported := func(v dsl.Var) bool {
+		return v.Text.Matches(`^\p{Lu}`)
+	}
+
 	m.Match(`type $x struct { $*_; sync.Mutex; $*_ }`).
-		Where(m["x"].Text.Matches(`^\p{Lu}`)).
+		Where(isExported(m["x"])).
 		Report("don't embed sync.Mutex")
 
 	m.Match(`type $x struct { $*_; *sync.Mutex; $*_ }`).
-		Where(m["x"].Text.Matches(`^\p{Lu}`)).
+		Where(isExported(m["x"])).
 		Report("don't embed *sync.Mutex")
 
 	m.Match(`type $x struct { $*_; sync.RWMutex; $*_ }`).
-		Where(m["x"].Text.Matches(`^\p{Lu}`)).
+		Where(isExported(m["x"])).
 		Report("don't embed sync.RWMutex")
 
 	m.Match(`type $x struct { $*_; *sync.RWMutex; $*_ }`).
-		Where(m["x"].Text.Matches(`^\p{Lu}`)).
+		Where(isExported(m["x"])).
 		Report("don't embed *sync.RWMutex")
 }
 
