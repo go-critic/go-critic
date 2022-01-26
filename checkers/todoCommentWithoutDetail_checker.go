62b188209b586220f2f625f51e033a5468920a32
Liam Galvin <liam@liam-galvin.co.uk>
2022-01-26 11:25:14 +0000

    Update todoCommentWithoutDetail_checker.go

--- checkers/todoCommentWithoutDetail_checker.go
+++ checkers/todoCommentWithoutDetail_checker.go
@@ -11,7 +11,7 @@ import (
 func init() {
 	var info linter.CheckerInfo
 	info.Name = "todoCommentWithoutDetail"
-	info.Tags = []string{"opinionated", "experimental"}
+	info.Tags = []string{"style", "opinionated", "experimental"}
 	info.Summary = "Detects TODO comments without detail/assignee"
 	info.Before = `
 // TODO
