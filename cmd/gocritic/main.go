8df9d7e15948e10a42b77b80ec1afd880b40d45f
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2022-08-10 00:21:33 +0300

    checkers: turn `init()` for embedded rules into exported func (#1246)
    The `checkers` package user should use `checkers.InitEmbeddedRules()`
    if ruleguard-based rules should be enabled.
    
    Fixes #1218

--- cmd/gocritic/main.go
+++ cmd/gocritic/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	_ "github.com/go-critic/go-critic/checkers" // Register go-critic checkers
+	"github.com/go-critic/go-critic/checkers" // Register go-critic checkers
 	"github.com/go-critic/go-critic/framework/lintmain"
 )
 
 var Version = "v0.0.0-SNAPSHOT"
 
 func main() {
+	checkers.InitEmbeddedRules()
 	lintmain.Run(lintmain.Config{
 		Name:    "gocritic",
 		Version: Version,
