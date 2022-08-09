dfd27d0df92629db2ef1c3958ae695e5132c066c
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2022-08-10 00:49:14 +0300

    checkers: use ctx.SizeOf instead of SizesInfo.SizeOf (#1247)
    Also add a ruleguard rule to avoid the unwanted sizeof form
    in the future.

--- rules.go
+++ rules.go
@@ -9,3 +9,8 @@ func nilSafeTypeOf(m dsl.Matcher) {
 	m.Match(`$_.ctx.TypesInfo.TypeOf($x)`).
 		Report(`use ctx.TypeOf($x) instead, it's nil-safe`)
 }
+
+func stdSizeof(m dsl.Matcher) {
+	m.Match(`$_.ctx.SizesInfo.Sizeof($_)`).
+		Report(`use ctx.SizeOf instead`)
+}
