2d2d8d89fe0fe64e25b40031d0bd7dbf6700938e
Denis <denislimarev@gmail.com>
2022-08-28 18:44:32 +0300

    add tests

--- checkers/testdata/errCheckInIf/positive_tests.go
+++ checkers/testdata/errCheckInIf/positive_tests.go
@@ -3,42 +3,73 @@ package checker_test
 type foo string
 
 func (foo) boo() error {
-    return nil
+	return nil
+}
+
+func (foo) boo2() (string, error) {
+	return "", nil
 }
 
 func boo() error {
-    return nil
+	return nil
+}
+
+func boo2() (string, error) {
+	return "", nil
 }
 
 func warning1() {
-    var err2 error
-    /*! returned error 'err' must be checked */
-    if err := boo(); err2 != nil {
-        print(err)
-    }
+	var err2 error
+	/*! returned error 'err' must be checked */
+	if err := boo(); err2 != nil {
+		print(err)
+	}
 
-    var err error
-    /*! returned error 'err' must be checked */
-    if err = boo(); err2 != nil {
-    }
+	var err error
+	/*! returned error 'err' must be checked */
+	if err = boo(); err2 != nil {
+	}
 
-    print(err)
+	print(err)
 }
 
 func warning2() {
-    var (
-        d    foo
-        err2 error
-    )
-    /*! returned error 'err' must be checked */
-    if err := d.boo(); err2 != nil {
-        print(err)
-    }
+	var (
+		d    foo
+		err2 error
+	)
+	/*! returned error 'err' must be checked */
+	if err := d.boo(); err2 != nil {
+		print(err)
+	}
 
-    var err error
     /*! returned error 'err' must be checked */
-    if err = d.boo(); err2 != nil {
-    }
+    if _, err := d.boo2(); err2 != nil {
+		print(err)
+	}
 
-    print(err)
+	var err error
+	/*! returned error 'err' must be checked */
+	if err = d.boo(); err2 != nil {
+	}
+
+    /*! returned error 'err' must be checked */
+    if _, err = d.boo2(); err2 != nil {
+	}
+
+	print(err)
+}
+
+func warning3() {
+	var (
+		err2 error
+	)
+	/*! returned error 'err' must be checked */
+	if _, err := boo2(); err2 != nil {
+		print(err)
+	}
+	/*! returned error 'err' must be checked */
+	if _, err := boo2(); err2 != nil {
+		print(err)
+	}
 }
